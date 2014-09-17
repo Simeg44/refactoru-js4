ca50ea125f9162280831fd87c3161fea6e1c15e9
Simeg44 <Simeg44@gmail.com>
2014-09-17 15:05:27 -0600

    added jquery support and forms

--- main2.js
+++ main2.js
@@ -12,19 +12,52 @@ var volunteers = {
 	street: []
 };
 
-/*var relocate = function(top, bottom) {
-	$(this).
-}*/
+// var relocate = function() {
+// 	return ($(this).closest(".people");
+// };
 
-$(document).ready(function() {
-	$(".people").on("click", ".btn", function() {
-		var name = $(this).closest(".people").find("#name").val();
+$(document).on('ready', function() {
+	$('.people').on('click', 'button', function(e) {
+		e.preventDefault();
+		// var phone = $(this).closest(".people").find("#phoneNum").val();
+		
+		victims.names.push($(this).closest(".people").find("#name").val());
+		victims.number.push($(this).closest(".people").find("#phoneNum").val());
+		victims.street.push($(this).closest(".people").find("#street").val());
+
+		victims.total++;
+
+		console.log(victims);
+
+		$('.people').hide();
+		$('.more-ppl').show();
+	});
+
+	$('.more-ppl').on('click', '.more', function(e) {
+		e.preventDefault();
+		$('.people').show();
+		$('.more-ppl').hide();
+	});
+
+	$('.more-ppl').on('click', '.done', function(e) {
+		e.preventDefault();
+		$('.more-ppl').hide();
+		$('.entry-change').show();
+	});
+})
+
+
+// $(document).ready(function() {
+// 	$(".btn").click(function() {
+// 		console.log('working');
+	
+// 	})
+	
+// })
+/*var name = $(this).closest(".people").find("#name").val();
 		victims.names.push(name);
 		// victims.number.push.val($("#phoneNum"));
 		// victims.street.push.val($("#street"));
 		victims.total++;
 
-		console.log(victims);
-	});
-	
-});
+		console.log(victims.names);*/
