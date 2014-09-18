a9c2dc6879539fd11aec29a0c11cecd6145e530d
Simeg44 <Simeg44@gmail.com>
2014-09-18 12:23:12 -0600

    Completed forms and displaying built database

--- main2.js
+++ main2.js
@@ -12,14 +12,32 @@ var volunteers = {
 	street: []
 };
 
-// var relocate = function() {
-// 	return ($(this).closest(".people");
-// };
+var clear = function() {
+	$('.people').find('input[type=text], textarea').val("");
+};
+
+var build = function(group) {
+	if (group === victims) {
+		$('#output-list').append('<h3>Victims</h3>');
+		$('#output-list').append("<li><ol id='victims-out'></ol></li>");
+
+		for(i = 0; i < group.total; i++) {
+			$('#victims-out').append("<li>" + group.names[i] + " ," + group.number[i] + " ," + group.street[i] + "</li>");
+		}
+	}
+	else {
+		$('#output-list').append('<h3>Volunteers</h3>')
+		$('#output-list').append("<li><ol id='volunteers-out'></ol></li>");
+
+		for(i = 0; i < group.total; i++) {
+			$('#volunteers-out').append("<li>" + group.names[i] + " ," + group.number[i] + " ," + group.street[i] + "</li>");
+		}
+	}
+};
 
 $(document).on('ready', function() {
-	$('.people').on('click', 'button', function(e) {
+	$(document).on('click', '#victims button', function(e) {
 		e.preventDefault();
-		// var phone = $(this).closest(".people").find("#phoneNum").val();
 		
 		victims.names.push($(this).closest(".people").find("#name").val());
 		victims.number.push($(this).closest(".people").find("#phoneNum").val());
@@ -33,16 +51,52 @@ $(document).on('ready', function() {
 		$('.more-ppl').show();
 	});
 
+	$(document).on('click', '#volunteers button', function(e) {
+		e.preventDefault();
+		
+		volunteers.names.push($(this).closest(".people").find("#name").val());
+		volunteers.number.push($(this).closest(".people").find("#phoneNum").val());
+		volunteers.street.push($(this).closest(".people").find("#street").val());
+
+		volunteers.total++;
+
+		console.log(volunteers);
+
+		$('.people').hide();
+		$('.more-ppl').show();
+	});
+
 	$('.more-ppl').on('click', '.more', function(e) {
 		e.preventDefault();
 		$('.people').show();
 		$('.more-ppl').hide();
+		clear();
 	});
 
 	$('.more-ppl').on('click', '.done', function(e) {
 		e.preventDefault();
 		$('.more-ppl').hide();
 		$('.entry-change').show();
+		clear();
+	});
+
+	$('.more-ppl').on('click', '.finished', function(e) {
+		e.preventDefault();
+		$('.more-ppl').hide();
+
+		build(victims);
+		build(volunteers);
+
+		$('.output').show();
+	});
+
+	$('.entry-change').on('click', 'button', function(e) {
+		e.preventDefault();
+		$('.people').attr('id', 'volunteers');
+		$('.done').removeClass('done').addClass('finished');
+		$('.people').show();
+		$('.entry-change').hide();
+		$('.people').find('h2').fadeToggle(0);
 	});
 })
 
